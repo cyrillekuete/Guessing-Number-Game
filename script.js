8db7b301aec6ac9b0c02fded41a2af425ef7ff44
Kuete Cyrille <cyrillekuete@gmail.com>
2023-01-13 13:46:01 +0100

    Assignment Completed

--- script.js
+++ script.js
@@ -11,20 +11,23 @@ document.querySelector('.guess').value = 46;
 
 // get the value when you click on the guess button
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '😒😒No Number!';
+
+    //When Player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👌😊 Correct Number🏆👌';
-    score = score + 1;
-    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
@@ -46,4 +49,17 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
